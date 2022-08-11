f3f919e401c351ce711a15ff23c11ff2c9f2c206
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-11 13:52:17 -0500

    Method POST create products :dollar:

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,23 +1,37 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getProducts(
+  get(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+    return {
+      message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    };
   }
 
   @Get('filter')
-  getProductFilter() {
-    return `yo soy un filter`;
+  getFilter() {
+    return {
+      message: `yo soy un filter`,
+    };
   }
 
   @Get(':id')
-  getProduct(@Param('id') id: string) {
-    return `product ${id}`;
+  getOne(@Param('id') id: string) {
+    return {
+      message: `product ${id}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
   }
 }
