cb7bbe737d71cf3eef17ac5d21adcf680bd5e041
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-11 22:01:10 -0500

    Implement service in controller :dragon_face:

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -14,17 +14,23 @@ import {
 
 import { Response } from 'express';
 
+import { ProductsService } from '../services/products.service';
+
 @Controller('products')
 export class ProductsController {
+  // en el constructor definir lo que se nesesita de este service
+  constructor(private productsService: ProductsService) {}
+
   @Get()
-  get(
+  getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return {
-      message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
-    };
+    // return {
+    //   message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    // };
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -37,32 +43,36 @@ export class ProductsController {
   // forma de express
   @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getOne(@Res() response: Response, @Param('id') id: string) {
-    response.status(200).send({
-      message: `product ${id}`,
-    });
+  getOne(@Param('id') id: string) {
+    // response.status(200).send({
+    //   message: `product ${id}`,
+    // });
+    return this.productsService.findOne(+id);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+    // return {
+    //   message: 'accion de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
-  update(@Param() id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+  update(@Param('id') id: string, @Body() payload: any) {
+    // return {
+    //   id,
+    //   payload,
+    // };
+    return this.productsService.update(+id, payload);
   }
 
   @Delete(':id')
   delete(@Param() id: number) {
-    return {
-      id,
-    };
+    // return {
+    //   id,
+    // };
+    return this.productsService.remove(id);
   }
 }
