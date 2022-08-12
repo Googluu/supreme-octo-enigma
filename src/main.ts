3aa2092207bebd9106443033cbb0cd0abf541e9a
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-12 08:44:19 -0500

    evitar params incorrectos que no este definidos en los dtos :no_entry_sign:

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // lo ignora quitar del payload todos los atributos que no esten definidos en el dtos
+      forbidNonWhitelisted: true, // con este no lo ingnora le pega su error para que envie lso datos good
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
