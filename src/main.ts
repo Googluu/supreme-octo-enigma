ea41d278fcdf2262a1ba84fbab8c4863adf3f993
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-15 21:51:40 -0500

    Integrando Swagger y PartialType con Open API :dvd:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +12,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // con este no lo ingnora le pega su error para que envie lso datos good
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('ecommerce')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,17 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { IsString, IsNotEmpty, Length, IsEmail } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, ApiProperty  } from "@nestjs/swagger";
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Length(6)
   readonly password: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   readonly role: string;
 }
