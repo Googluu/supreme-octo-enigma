44427687680b4679701aaa6101fa06f5c46fd729
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-15 21:13:43 -0500

    Validaciones de esquemas de variables de ambiente con Joi :dragon:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
-
 import { enviroments } from './enviroments';
 import config from './config';
 
@@ -17,6 +18,11 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env', // para que traiga el archivo en solicitado puede ser para desarrollo, production etc.. si no trae ninguno traer el archivo .env por defecto
       load: [config], // para cargar la configuracion de las variables de ambiente con tipado
       isGlobal: true, // para que sea global en toda la aplicacion
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     ProductsModule,
